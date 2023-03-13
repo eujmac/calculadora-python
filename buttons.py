96fa3db108ea541eabb4397b3ea680c27e2eb484
João Marcos <joaomarcosjmac@gmail.com>
2023-03-13 11:21:37 -0300

    capturando teclas ENTER, Backspace e ESC

--- buttons.py
+++ buttons.py
@@ -62,7 +62,13 @@ class ButtonsGrid(QGridLayout):
         self._equation = value
         self.info.setText(value)
 
+    def vouapagarvoce(self):
+        print('sinal recebido por', type(self).__name__)
+
     def _makeGrid(self):
+        self.display.eqPressed.connect(self.vouapagarvoce)
+        self.display.delPressed.connect(self.display.backspace)
+        self.display.clearPressed.connect(self.vouapagarvoce)
         for i, row in enumerate(self._gridMask):
             for j, buttonText in enumerate(row):
                 button = Button(buttonText)
--- display.py
+++ display.py
@@ -1,11 +1,17 @@
+from utils import isEmpty
 from PySide6.QtWidgets import QLineEdit
-from PySide6.QtCore import Qt
+from PySide6.QtCore import Qt, Signal
+from PySide6.QtGui import QKeyEvent
 
 # Import variáveis
 from variables import BIG_FONT_SIZE, TEXT_MARGIN, MINIMUM_WIDTH
 
 
 class Display(QLineEdit):
+    eqPressed = Signal()
+    delPressed = Signal()
+    clearPressed = Signal()
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.configStyle()
@@ -18,3 +24,33 @@ class Display(QLineEdit):
 
         self.setAlignment(Qt.AlignmentFlag.AlignRight)
         self.setTextMargins(*[TEXT_MARGIN for _ in range(4)])
+
+    def keyPressEvent(self, event: QKeyEvent) -> None:
+        text = event.text().strip()
+        key = event.key()
+        KEYS = Qt.Key
+
+        isEnter = key in [KEYS.Key_Enter, KEYS.Key_Return]
+        isDelete = key in [KEYS.Key_Backspace, KEYS.Key_Delete]
+        isEsc = key in [KEYS.Key_Escape]
+
+        if isEnter:
+            print('isEnter pressionado, sinal emitido', type(self).__name__)
+            self.eqPressed.emit()
+            return event.ignore()
+
+        if isDelete:
+            print('isDelete pressionado, sinal emitido', type(self).__name__)
+            self.delPressed.emit()
+            return event.ignore()
+
+        if isEsc:
+            print('isEsc pressionado, sinal emitido', type(self).__name__)
+            self.clearPressed.emit()
+            return event.ignore()
+
+        # não passar daqui se não tiver texto
+        if isEmpty(text):
+            return event.ignore()
+
+        print('Texto ', text)
